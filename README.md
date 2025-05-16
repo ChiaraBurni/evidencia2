944f68007383296f639f4a8232e1de31dc4ed6e3
ChiaraBurni <chiaraburni@gmail.com>
2025-05-16 13:45:23 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Evidencia de aprendizaje numero 2
+Nombre del proyecto: Sistema de Monitoreo de Contenido Hídrico de AgroTech Coop
+
+Alumnos: 
+Chiara Burni 41889108
+Juan Manuel Canales
+Valentina Gonzales
+Agustina Rojas
+Ivana Monsalvo
+
+
+Breve descripción: Menu interactivo que permite monitorear el contenido hidrico del suelo. 
+Posee como funiocnes: 
+. Registrar mediciones de contenido hídrico del suelo: el usuario ingresa datos de las mediciones del sensor en un lote (número, tipo de cultivo, porcentaje de humedad y fecha). Cada ingreso aumenta el contador de mediciones realizadas.
+
+. Evaluar el nivel de humedad del suelo: El usuario introduce un valor de humedad (en porcentaje) y se da una recomendación agronómica basada en rangos establecidos (ej: Entre 12% y 15%: humedad óptima → no se requiere acción.)
+
+. Consultar la cantidad de mediciones registradas: Mediante un contador se muestra cuantas mediciones se hicieron durante la sesión actual.
+
+instrucciones básicas de ejecución:
+1. Descarga o clona el proyecto
+2. Ejecuta el programa desde la terminal
+3. Utiliza el menu interactivo
---
+++ main.py
@@ -0,0 +1,51 @@
+opcion = ""
+
+#permite contar las mediciones
+contador_mediciones = 0
+
+while opcion != "4":
+    print("\nSistema de Monitoreo de Contenido Hídrico de AgroTech Coop")
+    print("\nMenu Principal: ")
+    print("1. Registrar medición de contenido hidrico del suelo")
+    print("2. Evaluación del nivel de humedad del suelo")
+    print("3. Total de mediciones registradas")
+    print("4. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    # registrar medicion
+    if opcion == "1":
+        print("\nRegistro del contenido hídrico del suelo (%): ")
+        lote = int(input("Ingrese el numero de lote: "))
+        cultivo = input("Ingrese el tipo de cultivo: ")
+        porcentaje = int(input("Ingrese el porcentaje de contenido hídrico del suelo (%): "))
+        fecha = input("Ingrese fecha y hora: ")
+        contador_mediciones += 1  
+
+    #recomendaciones segun humedad
+    elif opcion == "2":
+        print("\nEvaluación del Nivel de Humedad del Suelo")
+        humedad = input("Ingrese el porcentaje de contenido hídrico del suelo (%): ")
+
+        if humedad.isdigit():
+            humedad = int(humedad)
+            print("Contenido hidrico registrado:", humedad, "%")
+            if humedad < 12:
+                print("Recomendación: Humedad baja. Activar sistema de riego.")
+            elif humedad >= 12 and humedad <= 15:
+                print("Recomendación: Humedad optima. No hay recomendaciones")
+            else:
+                print("Recomendación: Humedad alta. Verificar drenaje del suelo y reducir la frecuencia de riego")
+        else:
+            print("Ingrese un numero entero.")
+
+    #cantidad de mediciones
+    elif opcion == "3":
+        print("\nTotal de mediciones registradas:", contador_mediciones)
+
+    #salir
+    elif opcion == "4":
+        print("\nSalir")
+    else:
+        print("Opción invalida. Intente nuevamente.")
+
